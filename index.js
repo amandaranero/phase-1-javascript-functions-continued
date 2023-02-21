920b4040e79f8368ec816e22a15af4cecd141871
amanda ranero <amandaranero@gmail.com>
2023-02-21 16:59:09 -0500

    code forjavascript function cont

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity ='roller-skate'){
+   return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(thing = "go to the office"){
+    return `This Monday, I will ${thing}.`;
+}
+
+function wrapAdjective(flair = "!"){
+    return function (name = "special"){
+        return `You are ${flair}${name}${flair}!`;
+    }
+
+}
